0cfc6cf9bcb793bc68ae264a387679817a6e7512
mattuk <makampf@gmail.com>
2011-01-14 08:14:58 +0100

    finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,4 +17,8 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "About")
   end
 
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
